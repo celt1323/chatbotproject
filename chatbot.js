739989a0444a64dd4d9279cb7fa87714382b6c45
Celeste Tan <houseoftan4@gmail.com>
2016-09-27 08:57:10 -0700

    added 1 more question

--- chatbot.js
+++ chatbot.js
@@ -2,14 +2,20 @@ var responses = {
  'name': 'im skye',
 'age': 'im a computer',
 'hi': 'hello',
-'morning' : 'Good morning'
+'morning' : 'Good morning',
+'hows the weather' : 'sunny'
 };
 
 
 function speak(){
 
   var question = $("#input").val();
-  var answer = responses[question];
+var answer = responses[question];
   $("#chat-area").html(answer);
 
+
+
 }
+
+  // $("#chat-area").prepend(responses);
+  // $("#input").append(question);
