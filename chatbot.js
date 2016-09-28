8374fb6ed84374833bf89aeacbecd1e58f32b88a
Celeste Tan <houseoftan4@gmail.com>
2016-09-28 08:26:43 -0700

    working on fallback response and time

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,10 @@
+function returnTime(){
+var now = new Date(Date.now());
+var hours = now.getHours();
+// returnTime(hours);
+}
+
+
 var responses = {
  'name': 'im skye',
 'age': 'im a computer',
@@ -7,17 +14,27 @@ var responses = {
 'how are you' : 'im good',
 'whats 8273 * 5' : '41365',
 'im bored' : 'ok?',
-'what time is it' : '9:03am',
+'what time is it' : "It is currently " + returnTime() + ".",
 'whats todays date' : 'september 27, 2016'
 };
 
+// var fallbackResponse = {
+//   'ummmm'
+//   'i dont know what you mean'
+//   '????'
+// }
+
 
 function speak(){
+// if(responses == undefined){
+//   var fallbackResponse = responses[question];
+//     $("#chat-area").html(fallbackResponse);
+// }
+
 
   var question = $("#input").val();
 var answer = responses[question];
   $("#chat-area").html(answer);
-
 
 
 }
